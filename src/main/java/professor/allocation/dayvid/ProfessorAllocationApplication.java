2b7c3b90ff38f7e703cd0e1f9df424f6891de535
dayviddouglas <dayvidacademico@gmail.com>
2025-11-10 14:18:08 -0300

    Ajustando o nome do projeto

--- src/main/java/professor/allocation/dayvid/ProfessorAllocationApplication.java
+++ src/main/java/professor/allocation/dayvid/ProfessorAllocationApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProfessorAllocationDayvidApplication {
+public class ProfessorAllocationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProfessorAllocationDayvidApplication.class, args);
+		SpringApplication.run(ProfessorAllocationApplication.class, args);
 	}
 
 }
